9e8280e67045007501ac92ac85d877b0d1c8265f
Polamarasetti <b.a.polamarasetti@accenture.com>
2025-03-22 17:34:58 +0530

    added some extra endpoints

--- app.js
+++ app.js
@@ -41,6 +41,45 @@ app.post('/books', async (req, res) => {
   res.json(book);
 });
 
+app.get('/authors', async (req, res) => {
+  const authors = await Author.findAll();
+  if (authors.length === 0) {
+    return res.status(404).json({ error: "No authors found" });
+  }
+  res.status(200).json(authors);
+});
+
+app.post('/author/new', async (req, res) => {
+  try {
+    const newAuthor = await Author.create(req.body);
+    res.status(200).json({message: 'new author data created successfully', newAuthor});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+/*app.get('/genres/:genreId/authors', async (req, res) => {
+  try {
+    const authorsByGenreId = await Book.findAll({ where: { genreId: req.params.genreId } });
+    if (authorsByGenreId.length === 0) {
+      return res.status(404).json({ error: "No authors found for this genre" });
+    }
+    res.status(200).json({ 'Authors': authorsByGenreId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});*/
+
+app.get('/genres/:genreId/authors', async (req, res) => {
+  try {
+    let genres = await Genre.findByPk(req.params.genreId);
+    if (!genres) return res.status(404).json({ error: "Genre not found" });
+    //let Authors = genres.forEach(genre => genre.)
+    res.json(genres);
+  } catch (err) {
+    res.status(500).json({ error: err.stack });
+  }
+});
 
 const PORT = 3010;
 app.listen(PORT, () => {
